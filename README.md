17a0b3c96377eaa987b81f42d8b4941b7ed2e8d6
Alex Mykyta <amykyta3@users.noreply.github.com>
2020-04-05 11:01:16 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/ralbot.svg)](https://pypi.org/project/ralbot)
+
+# RALBot
+
+How exciting! Finally a command-line tool that unifies all the RALBot tools!
+
+This is simply a placeholder for now as this is still in development.
+
+Check this [issue thread for a summary of features](https://github.com/SystemRDL/RALBot/issues/1). Feel free to comment if you have ideas!
---
+++ setup.py
@@ -0,0 +1,48 @@
+import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+with open(os.path.join("ralbot", "__about__.py")) as f:
+    v_dict = {}
+    exec(f.read(), v_dict)
+    version = v_dict['__version__']
+
+setuptools.setup(
+    name="ralbot",
+    version=version,
+    author="Alex Mykyta",
+    author_email="amykyta3@github.com",
+    description="Command-line tool for control/status register automation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SystemRDL/RALBot",
+    packages=['ralbot'],
+    include_package_data=True,
+    python_requires='>=3.4',
+    install_requires=[
+        # FIXME: Add dependencies
+    ],
+    classifiers=(
+        "Development Status :: 1 - Planning", # FIXME: move to production when ready
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+    ),
+    project_urls={
+        #"Documentation": "TBD", # FIXME: Point to readthedocs page
+        "Source": "https://github.com/SystemRDL/RALBot",
+        "Tracker": "https://github.com/SystemRDL/RALBot/issues",
+    },
+)
